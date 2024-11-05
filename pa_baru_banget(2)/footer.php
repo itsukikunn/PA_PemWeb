cc0121704aa5b4a38f3d6b9ebc83e39146b2ce53
Flugel <151924761+Eu4ngelion@users.noreply.github.com>
2024-11-05 23:47:37 +0800

    Add files via upload

---
+++ pa_baru_banget(2)/footer.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<footer class="footer-main">
+    <div class="footer-container">
+        <div class="footer-content">
+            <div class="footer-item">
+                <div>
+                    <div class="footer-option">
+                        <img src="uploads/logo.png" alt="Sempajahaven" class="custom-logo">
+                        <div>
+                            <h5 class="title-option"> SHOP </h5>
+                            <ul class="list-option">
+                                <li class="list-item">
+                                    <a href="index.html" class="list-link">Home</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="start_review.html" class="list-link">Start Selling</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="about_me.html" class="list-link">About Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 class="title-option"> SELL </h5>
+                            <ul class="list-option">
+                                <li class="list-item">
+                                    <a href="index.html" class="list-link">How To Sell</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="start_review.html" class="list-link">Verification User</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 class="title-option"> SUPPORT </h5>
+                            <ul class="list-option">
+                                <li class="list-item">
+                                    <a href="index.html" class="list-link"> Contact Us</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="start_review.html" class="list-link">Help Center</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 class="title-option"> COMPANY & POLICIES</h5>
+                            <ul class="list-option">
+                                <li class="list-item">
+                                    <a href="index.html" class="list-link">About Us</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="start_review.html" class="list-link">Policy Center</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 class="title-option"> FIND US ON </h5>
+                            <ul class="list-option">
+                                <li class="list-item">
+                                    <a href="index.html" class="list-link">Facebook</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="start_review.html" class="list-link">Instagram</a>
+                                </li>
+                                <li class="list-item">
+                                    <a href="start_review.html" class="list-link">Twitter</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <div class="copyright">
+                    <div class="sub-copyright">
+                        <p>© 2024 Sempajahaven. All Rights Reserved</p>
+                        <ul class="menu-copyright">
+                            <li class="list-item">
+                                <a href="index.html" class="list-link">Terms of Service</a>
+                            </li>
+                            <li class="list-item">
+                                <a href="start_review.html" class="list-link">Privacy Policy</a>
+                            </li>
+                            <li class="list-item">
+                                <a href="start_review.html" class="list-link">Accessibility</a>
+                            </li>
+                            <li class="list-item">
+                                <a href="start_review.html" class="list-link">Cookie Preferences</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ pa_baru_banget(2)/history.php
@@ -0,0 +1,110 @@
+<?php
+require "koneksi.php";
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['username'])) {
+    echo "<script>
+    alert('Anda harus login terlebih dahulu')
+    window.location.href='login.php'
+    </script>";
+    exit();
+} else if (isset($_SESSION['admin'])){
+    if ($_SESSION["admin"] == true) {
+        echo "<script>
+        window.location.href='CRUDadmin.php';
+        </script>";
+    }
+}
+$id_user = $_SESSION['id_user'];
+$image_dir = "uploads/";
+
+
+// SELECT t.*, b.nama_buku, b.harga_buku, b.gambar 
+// LEFT JOIN buku b ON t.FK_id_buku = b.id_buku 
+// Join transaksi dan buku
+$history = [];
+$query = "SELECT t.*, b.nama_buku, b.harga_buku, b.gambar 
+          FROM transaksi t 
+          LEFT JOIN buku b ON t.FK_id_buku = b.id_buku 
+          WHERE t.FK_id_user = '$id_user' 
+          AND t.status_transaksi = 2 
+          ORDER BY t.tanggal DESC";
+$result = mysqli_query($conn, $query);
+while ($row = mysqli_fetch_array($result)) {
+    $history[] = $row;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histori</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="styles/history.css">
+
+</head>
+
+<body>
+    <?php include "navbar.php"; ?>
+
+    <div class="judul">
+        <h1>Histori Pembelian</h1>
+    </div>
+
+    <section class="isi-halaman">
+        <div class="list-history">
+            <?php if (empty($history)): ?>
+                <p>Tidak Ada Histori Pembelian.</p>
+            <?php else: ?>
+                <?php foreach ($history as $item): ?>
+                    <div class="item-history">
+                        <div class="gambar-history">
+                            <a href="detail_buku.php?id_buku=<?php echo $item['FK_id_buku']; ?>">
+                                <img src="<?php echo $image_dir . $item['gambar']; ?>" alt="Buku sudah tidak tersedia">
+                            </a>
+                        </div>
+                        <div class="detail-history">
+                            <!-- Judul Buku -->
+                            <h3><?php if ($item["FK_id_buku"] == null) {
+                                    echo "Buku sudah tidak tersedia";
+                                } else {
+                                    echo $item['nama_buku'];
+                                }
+                                ?></h3>
+                            <!-- Harga Buku -->
+                            <p>
+                                <?php
+                                if ($item['FK_id_buku'] == null) {
+                                    echo "";
+                                } else {
+                                    echo "Rp" . number_format($item['harga_buku'], 0, ',', '.');
+                                }
+                                ?>
+                            </p>
+                            <!-- Kuantitas -->
+                            <p>Kuantitas: <?php echo $item['jumlah_transaksi']; ?></p>
+                            <!-- Total Harga -->
+                            <b>
+                                <?php
+                                if ($item['FK_id_buku'] == null) {
+                                    echo '';
+                                } else {
+                                    echo "<p>Total: Rp" . number_format($item['jumlah_transaksi'] * $item['harga_buku'], 0, ',', '.') . "</p>";
+                                }
+                                ?>
+                            </b>
+                            <p>Tanggal Transaksi: <?php echo $item['tanggal']; ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+    </section>
+</body>
+</html>
