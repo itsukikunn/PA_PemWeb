193c097d50f2a023752187b560f6e2a7586889b4
Flugel <151924761+Eu4ngelion@users.noreply.github.com>
2024-11-08 19:49:12 +0800

    Delete PA_FIX directory

--- PA_FIX/scripts/navbar.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function () {
-    const headerButtons = document.querySelector(".header-buttons");
-    if (headerButtons) {
-        headerButtons.classList.toggle("active");
-    }
-})
